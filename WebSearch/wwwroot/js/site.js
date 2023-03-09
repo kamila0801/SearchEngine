60014b9cfa7c251626ca7ceb16d893945195f28a
kamila0801 <km@danskscanning.dk>
2023-03-09 09:30:37 +0100

    added presentation layer

---
+++ WebSearch/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let ViewModel = function () {
+
+    let me = this;
+    
+    me.searchTerms = ko.observable();
+    me.hits = ko.observable();
+    me.results = ko.observableArray();
+    me.timeUsed = ko.observable();
+    
+    me.search = function() {
+        $.ajax({
+            url: "http://localhost:8071/search/" + me.searchTerms() + "/10",
+            success: function(data) {
+                data = JSON.parse(data);
+                console.log(data);
+                me.hits(data.Hits);
+                me.timeUsed(data.TimeUsed);
+                me.results.removeAll();
+                data.DocumentHits.forEach(function(hit) {
+                    me.results.push(hit);
+                });
+                console.log(me.hits());
+                console.log(me.timeUsed());
+            }
+        });
+    }
+
+};
+ko.applyBindings(new ViewModel());
